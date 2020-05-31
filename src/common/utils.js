6e7a7feede106d388f63931a3f545cdd873127e7
Hendry Yuwono <ndry93@yahoo.co.id>
2020-05-31 23:49:29 +0700

    add default axios header and timeout

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,9 +1,27 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+const { logger } = require('./logger')(__filename);
+
+const requestConfig = {
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	},
+	timeout: 60000 // timeout in 1 min
+};
+
 async function fetchHTML(url) {
-	const { data } = await axios.get(url);
-	return cheerio.load(data);
+	logger.info('fetchHTML:', url);
+	try {
+		const { data } = await axios.get(url, requestConfig);
+		logger.info('fetchHTML data:', data);
+		const result = cheerio.load(data);
+		logger.info('fetchHTML cheerio:', result);
+		return result;
+	} catch (error) {
+		logger.error('fetchHTML error:', error);
+	}
+	return null;
 }
 
 function getCurrentTimestamp() {
